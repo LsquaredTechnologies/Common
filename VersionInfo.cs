45aca3079127720290acbcae6d26c369a3fb7a5f
Lionel L <lionel.lalande@lsquared.fr>
2015-06-03 21:24:36 +0200

    Add common files

---
+++ VersionInfo.cs
@@ -0,0 +1,8 @@
+// <copyright file="VersionInfo.cs" company="LSQUARED">
+// Copyright © 2008-2014
+// </copyright>
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0-unstable-r1")]
