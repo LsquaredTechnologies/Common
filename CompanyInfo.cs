7298ed26a7cbf64dc6003148045fc25dbc47861d
Lionel L <lionel.lalande@lsquared.fr>
2015-06-03 21:53:58 +0200

    Correct MSBuild files and add CompanyInfo.cs

---
+++ CompanyInfo.cs
@@ -0,0 +1,10 @@
+// <copyright file="AssemblyInfo.cs" company="LSQUARED">
+// Copyright © 2008-2015
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Lsquared")]
+[assembly: AssemblyCopyright("Copyright © Lsquared 2015")]
+[assembly: AssemblyTrademark("")]
